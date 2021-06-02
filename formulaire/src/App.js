72270eb50a5426ddddb335347277a736f095e7c7
imen <imen.mhamdi.19@gmail.com>
2021-06-02 14:53:16 +0100

    first commit

---
+++ formulaire/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Formulaire from './formulaire'
+import index from './index'
+function App() {
+  return (
+    <div className="App">
+      <Formulaire />
+    </div>
+  );
+}
+
+export default App;
